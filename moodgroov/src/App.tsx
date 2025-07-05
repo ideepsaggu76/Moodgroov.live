3477c46cea4e8adf42ad0c702d44e8ae77f4eb1c
instagram-clone <instagram-clone@example.com>
2025-07-05 18:10:59 +0530

    Add Dashboard with transparent background and dynamic music player

--- moodgroov/src/App.tsx
+++ moodgroov/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 import './styles/global.css';
 
 const App: React.FC = () => (
-  <div className="app">
-    <Home />
-  </div>
+  <Router>
+    <div className="app">
+      <nav className="app-nav">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/dashboard" className="nav-link">Dashboard</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
